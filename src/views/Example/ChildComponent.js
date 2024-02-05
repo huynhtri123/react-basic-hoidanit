cf5e1d570d92c860e53e32ada482615362a766b2
huynhtri123 <huynhmtri1211@gmail.com>
2024-02-05 20:25:50 +0700

    video14: conditional output - show/hide

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,21 +1,57 @@
 import React from 'react';
 
-//Function component
-const ChildComponent = (props) => {
-    let { arrayJobs } = props;
-    return (
-        <div className='job-lists'>
-            {
-                arrayJobs.map((item, index) => {
-                    return (
-                        <div key={item.id}>
-                            {item.title} - {item.salary}
+// 2 components: class component / function component (func, arrow func)
+//JSX: thay vi tra ve 1 bien thi no tra ve 1 dong html (code html trong file js)
+//JSX giup viet js trong the html (sd {})
+//JSX: return 1 Block
+//Fragment: <> </> 
+
+class ChildComponent extends React.Component {
+
+    state = {
+        showJobs: false
+    }
+    handleShowHide = () => {
+        this.setState({
+            showJobs: !this.state.showJobs
+        })
+    }
+
+    render() {
+
+        let { arrayJobs } = this.props;
+        let { showJobs } = this.state;
+        let check = showJobs === true ? 'showJobs = true' : 'showJobs = false';
+        console.log('>>>check:', check)
+
+        return (
+            <>
+                {showJobs === false ?
+                    <div>
+                        <button onClick={() => this.handleShowHide()}>Show</button>
+                    </div>
+                    :
+                    <>
+                        <div className='job-lists'>
+                            {
+                                arrayJobs.map((item, index) => {
+                                    return (
+                                        <div key={item.id}>
+                                            {item.title} - {item.salary}$
+                                        </div>
+                                    )
+                                })
+                            }
                         </div>
-                    )
-                })
-            }
-        </div>
-    )
+                        <div>
+                            <button onClick={() => this.handleShowHide()}>Hide</button>
+                        </div>
+                    </>
+                }
+            </>
+        )
+
+    }
 }
 
 export default ChildComponent;
