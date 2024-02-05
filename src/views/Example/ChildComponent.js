c37b962a55081825eead71466c1fe38b2c655cd8
huynhtri123 <huynhmtri1211@gmail.com>
2024-02-05 19:48:08 +0700

    video13: stateless/statefull - function component

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,60 +1,21 @@
 import React from 'react';
 
-// 2 components: class component / function component (func, arrow func)
-//JSX: thay vi tra ve 1 bien thi no tra ve 1 dong html (code html trong file js)
-//JSX giup viet js trong the html (sd {})
-//JSX: return 1 Block
-//Fragment: <> </> 
-
-class ChildComponent extends React.Component {
-
-    state = {
-        firstName: '',
-        lastName: ''
-    }
-
-    handleChangeFirstName = (event) => {
-        this.setState({
-            firstName: event.target.value
-        })
-    }
-    handleChangeLastName = (event) => {
-        this.setState({
-            lastName: event.target.value
-        })
-    }
-    handleSubmit = (event) => {
-        event.preventDefault()
-        console.log('>>>check data:', this.state)
-    }
-
-    render() {
-        // console.log('>>>check props:', this.props);
-        // return (
-        //     <>
-        //         <div>Child component: {this.props.name}</div>
-        //     </>
-        // )
-
-        //Object destructuring
-        console.log('>>>check props:', this.props)
-
-        let { name, age, address, arrayJobs } = this.props; //name, age la ten 9 xac 2 thuoc tinh cua props
-        return (
-            <div className='job-lists'>
-                {
-                    arrayJobs.map((item, index) => {
-                        return (
-                            <div key={item.id}>
-                                {item.title} - {item.salary}
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
-
-    }
+//Function component
+const ChildComponent = (props) => {
+    let { arrayJobs } = props;
+    return (
+        <div className='job-lists'>
+            {
+                arrayJobs.map((item, index) => {
+                    return (
+                        <div key={item.id}>
+                            {item.title} - {item.salary}
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
 }
 
 export default ChildComponent;
