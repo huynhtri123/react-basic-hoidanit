0cbad6b7e990d60f1955634e4ce23752a0283008
huynhtri123 <huynhmtri1211@gmail.com>
2024-02-05 09:49:26 +0700

    video5: state

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,10 +1,41 @@
 import React from 'react';
 
+// 2 components: class component / function component (func, arrow func)
+//JSX: thay vi tra ve 1 bien thi no tra ve 1 dong html (code html trong file js)
+//JSX giup viet js trong the html (sd {})
+//JSX: return 1 Block
+//Fragment: <> </> 
+
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Tri1',
+        age: 20
+    }
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
     render() {
+
+        let name = "Tri";
+
         return (
-            <div>Hello hihi, this is my component</div>
+            <>
+                <div className='first'>
+                    {console.log('My name is :', name)}
+                    <input value={this.state.name} type='text' onChange={(event) => { this.handleOnChangeName(event) }}></input>
+                    Hello components, my name is {this.state.name}
+                </div>
+                <div className='second'>
+                    My age: {this.state.age}
+                </div>
+            </>
         )
+
     }
 }
 
