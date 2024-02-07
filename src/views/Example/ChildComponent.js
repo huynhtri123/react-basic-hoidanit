5fd1319d55b91736ba94aece5e314de588e7e741
huynhtri123 <huynhmtri1211@gmail.com>
2024-02-07 20:31:09 +0700

    video19: css cho react (dung scss)

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Demo.scss";
 
 class ChildComponent extends React.Component {
 
@@ -22,7 +23,10 @@ class ChildComponent extends React.Component {
             <>
                 {showJobs === false ?
                     <div>
-                        <button onClick={() => this.handleShowHide()}>Show</button>
+                        <button className="btn-show"
+                            onClick={() => this.handleShowHide()}>
+                            Show
+                        </button>
                     </div>
                     :
                     <>
